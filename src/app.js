d647053901f734d3b8f2719d4ae134f1d60d0faf
AngeZambrano <129020174+AngeZambrano@users.noreply.github.com>
2023-06-07 12:21:46 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,43 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  function randomNumber() {
+    let numbers = [
+      "A",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let indexNumber = Math.floor(Math.random() * numbers.length);
+    return numbers[indexNumber];
+  }
+
+  function randomSuit() {
+    let suits = ["♦", "♥", "♠", "♣"];
+    let indexSuit = Math.floor(Math.random() * suits.length);
+
+    if (suits[indexSuit] === "♦" || suits[indexSuit] === "♥") {
+      suits[indexSuit] = `<span style="color: red;">${suits[indexSuit]}</span>`;
+    }
+
+    return suits[indexSuit];
+  }
+
+  let finalNumber = randomNumber();
+  document.querySelector(".number").innerHTML = finalNumber;
+
+  let palos = randomSuit();
+  document.querySelector(".card-top").innerHTML = palos;
+  document.querySelector(".card-bottom").innerHTML = palos;
 };
